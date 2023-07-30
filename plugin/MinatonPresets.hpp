193fb1b9ea01207b555e87a5408540adb56bd423
AnClark Liu <anclarkliu@outlook.com>
2023-07-30 22:33:45 +0800

    Add preset manager & built-in patch support
    - Add new class MinatonPresetManager, for loading presets
    - Add embedded preset (bank) struct definitions and loader
    - Add utility factory_bank_to_c (adapted from Triceratops-XT) and
      corresponding CMake rules

---
+++ plugin/MinatonPresets.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+// Forward decls.
+namespace DISTRHO {
+class MinatonUI;
+}
+
+struct EmbedPresetInfo {
+    std::string name;
+    std::string file_path;
+    bool read_only;
+
+    const unsigned int* file_array = NULL;
+    int file_array_size = 0;
+};
+
+void InitializeEmbedFactoryPresets(std::vector<EmbedPresetInfo>& factory_presets_list);
+
+class MinatonPresetManager {
+    std::vector<EmbedPresetInfo> fEmbedFactoryPresets;
+    DISTRHO::MinatonUI* fUiInstance;
+
+public:
+    MinatonPresetManager(DISTRHO::MinatonUI*);
+    ~MinatonPresetManager();
+
+    // -------------------------------------------------------------------
+    // Load patch
+
+    int loadPatchById(uint32_t);
+    void loadDefaultPatch();
+
+    // -------------------------------------------------------------------
+    // Query built-in preset
+
+    uint32_t getEmbedPresetCount();
+    EmbedPresetInfo getEmbedPresetById(uint32_t);
+    std::vector<EmbedPresetInfo>& getEmbedPresetBank();
+
+private:
+    // -------------------------------------------------------------------
+    // Internal helpers
+
+    // Worker for parsing and applying actual patch file
+    void _loadPatch(const std::string&);
+
+    // Set parameter, then update UI
+    void _applyParameter(uint32_t, float);
+};
