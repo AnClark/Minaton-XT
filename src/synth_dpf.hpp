c455ae4660fd2f66ab98a57985d1c4e67fa502c7
AnClark Liu <anclarkliu@outlook.com>
2023-09-20 13:18:53 +0800

    synth: Optimize checks for channel outputs
    Before, channel output checks frequently calls
    fSynthesizer->get_dco_output_channel(), which consumes CPU time. However
    some function calls can be merged to a single call, so I can reduce CPU
    usage, and improve performance.
    
    Improvements:
    
    * Do not use separate variables for channel modes. Use array instead.
      So I can save CPU time by getting rid of `switch` statement.
    
    * Replace IS_DCO_OUT_TO_(L|R) macros with class method.
      Directly check output channel within class, instead of calling
      get_dco_output_channel() twice for checking CHANNEL_LEFT and
      CHANNEL_L_AND_R.
      The two methods are is_dco_output_to_(L|R). For performance, they are
      inline functions.
    
    * Use `const uint8_t` for (get|set)_dco_output_channel().

--- src/synth_dpf.hpp
+++ src/synth_dpf.hpp
@@ -53,9 +53,12 @@ public:
     int add_wave(string, const unsigned char*, size_t size);
 
     void set_output_mode(bool mode) { output_mode = minaton_output_mode(mode); }
-    void set_dco_output_channel(int dco_number, minaton_channel_mode channel);
+    void set_dco_output_channel(const uint8_t dco_number, minaton_channel_mode channel);
     minaton_output_mode get_output_mode() { return output_mode; }
-    minaton_channel_mode get_dco_output_channel(int dco_number);
+    minaton_channel_mode get_dco_output_channel(const uint8_t dco_number);
+
+    inline bool is_dco_out_to_L(const int dco_number) { return (dco_output_channel[dco_number] == CHANNEL_LEFT) || (dco_output_channel[dco_number] == CHANNEL_L_AND_R); }
+    inline bool is_dco_out_to_R(const int dco_number) { return (dco_output_channel[dco_number] == CHANNEL_RIGHT) || (dco_output_channel[dco_number] == CHANNEL_L_AND_R); };
 
     void panic();
     void reset_dco_out_position();
@@ -64,7 +67,5 @@ private:
     Memory m_memory;
 
     minaton_output_mode output_mode; // true: stereo; false: mono
-    minaton_channel_mode dco1_output_channel;
-    minaton_channel_mode dco2_output_channel;
-    minaton_channel_mode dco3_output_channel;
+    minaton_channel_mode dco_output_channel[3];
 };
